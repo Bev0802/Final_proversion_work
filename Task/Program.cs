2b07c4e4ded0b85f8a0efae19e87a37513c1d60b
Katy <bev08021982@gmail.com>
2022-12-02 17:27:30 +0400

    Написала метод создания массива через ввод из консоли и его печати

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,5 +4,31 @@
 //["1234","1567","-2","computer science"] -> ["-2"]
 //["Russia", "Denmark","Kazan"]` -> []
 
-Console.WriteLine("Hello, World!");
+string[] ArrayStrings(int size)
+{
+    string[] arrStr = new string[size];
+    Console.WriteLine($"Введите {size} слов(a) для заполения массива");
+
+    for (int i = 0; i < arrStr.Length; i++)  
+    {
+        string? word = Console.ReadLine();
+        if (word != null) arrStr[i] = word;
+    }
+    return arrStr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+        if (i < arr.Length - 1) Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+string[] arrayStrings = ArrayStrings(3);
+PrintArray(arrayStrings);
+
 
